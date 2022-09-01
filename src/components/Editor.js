43bbf9ca325b7a3a0b02ea1b231cc8fe124ccef1
eriknastesjo <erna21@student.bth.se>
2022-09-01 15:14:08 +0100

    Add toolbar above editor.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,35 +1,64 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Parse from 'html-react-parser';
+import { FaSave } from 'react-icons/fa';
+import { IconContext } from 'react-icons'
+
 
 function Editor () {
 
     const [content, setContent] = useState("");
+    const [col, setCol] = useState("#272727");
+    const originalCol = "#272727";
 
     return (
-        <div className="ContainerCenter">
-            <div className="Editor">
-                <h2>Eriks editor</h2>
-                <CKEditor
-                    editor={ClassicEditor}
-                // data="<p>Hello from CKEditor 5!</p>"
-                // onReady={editor => {
-                //     // You can store the "editor" and use when it is needed.
-                //     console.log('Editor is ready to use!', editor);
-                // }}
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    // <div dangerouslySetInnerHTML={{ data }} />
-                    // setContent(dangerouslySetInnerHTML(data));
-                    // console.log({ data });
-                }}
-                // onBlur={(event, editor) => {
-                //     console.log('Blur.', editor);
-                // }}
-                // onFocus={(event, editor) => {
-                //     console.log('Focus.', editor);
-                // }}
-                />
+        <div>
+            <div className="toolbar">
+                <IconContext.Provider value={{ color: col }}>
+                    <FaSave size={30}
+                        className="toolIcon"
+                        onMouseEnter={() => {
+                            setCol("white");
+                        }}
+                        onMouseLeave={() => {
+                            setCol(originalCol);
+                        }}
+                        onClick={() => {
+                            console.log(content);
+                        }}
+                    />
+                </IconContext.Provider>
+            </div>
+            <div className="edit-prev-container">
+                <div className="editor">
+                    <h1>Eriks editor</h1>
+                    <CKEditor
+                        editor={ClassicEditor}
+                    // data="<p>Hello from CKEditor 5!</p>"
+                    // onReady={editor => {
+                    //     // You can store the "editor" and use when it is needed.
+                    //     console.log('Editor is ready to use!', editor);
+                    // }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        setContent(data);
+                        // <div dangerouslySetInnerHTML={{ data }} />
+                        // setContent(dangerouslySetInnerHTML(data));
+                        // console.log({ data });
+                    }}
+                    // onBlur={(event, editor) => {
+                    //     console.log('Blur.', editor);
+                    // }}
+                    // onFocus={(event, editor) => {
+                    //     console.log('Focus.', editor);
+                    // }}
+                    />
+                </div>
+                <div>
+                    <h2>Preview</h2>
+                    <div>{Parse(content)}</div>
+                </div>
             </div>
         </div>
     );
