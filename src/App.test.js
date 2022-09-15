20e2892c592ded117eb10158fa29ba798b676440
Erik Nästesjö Todd <eriknastesjo@hotmail.com>
2022-09-15 16:00:32 +0200

    Change temporarily to local for testing. Add three very simple tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to Eriks Editor/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders button with text "New Document"', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/New Document/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders drop-down list with text "Choose a document"', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Choose a document/i);
   expect(linkElement).toBeInTheDocument();
 });
