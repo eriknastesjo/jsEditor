7e2c1399565cca600cb888e56415deae58fc68db
eriknastesjo <erna21@student.bth.se>
2022-09-24 08:30:02 +0100

    Structuring code.

--- src/components/editor.js
+++ src/components/editor.js
@@ -12,41 +12,79 @@ import 'react-quill/dist/quill.snow.css';
 function Editor(props) {
 
     const [name, setName] = useState("");
+
     const [content, setContent] = useState("");
     // const [editor, setEditor] = useState(null);
     let updateCurrentDocOnChange = true;
 
-    // =====================================
+
+    // SKICKA VIA SOCKET TILL SERVER
+    // =========================================
+
+    // useEffect(() => {
+    //     const data = {
+    //         _id: props.currentDoc._id,
+    //         name: name
+    //     }
+    //     // console.log(currDoc);
+    //     props.socket.emit("name", data);
+
+    //     // props.socket.emit("content", content);
+    // }, [name]);
+
+    useEffect(() => {
+        const data = {
+            _id: props.currentDoc._id,
+            content: content
+        }
+        // console.log(currDoc);
+        props.socket.emit("content", data);
+
+        // props.socket.emit("content", content);
+    }, [content]);
+
+
+    // TA EMOT VIA SOCKETS FRÅN SERVER
+    // =========================================
+
+    // props.socket.on("name", function (data) {
+    //     setEditorName(data.name, false);
+    // });
 
     props.socket.on("content", function (data) {
-        setEditorContent(data, false);
+        setEditorContent(data.content, false);
     });
 
-    props.socket.on("content", function (data) {
-        setEditorContent(data, false);
-    });
+
+    // ONCHANGE FUNKTIONER
+    // =========================================
+
+    function changeName(event) {
+        if (event.target !== undefined && updateCurrentDocOnChange) {
+            setName(event.target.value);
+        }
+        updateCurrentDocOnChange = true;
+    }
 
     function changeContent(html) {
         if (updateCurrentDocOnChange) {
-            // const copy = Object.assign({}, currentDoc);
-
-            // copy.html = html;
-
-            // setCurrentDoc(copy);
             setContent(html);
-            // props.setCurrentContent(html);
         }
 
         updateCurrentDocOnChange = true;
     }
 
-    function setEditorName(name, triggerChange) {
-        let editName = document.getElementsByClassName('name')[0];
 
-        updateCurrentDocOnChange = triggerChange;
-        editName.value = name;
-        updateCurrentDocOnChange = triggerChange;
-    }
+    // FUNKTIONER SOM MÖJLIGGÖR ÄNDRING UTAN ATT ÄNDRA STATES
+    // =========================================
+
+    // function setEditorName(name, triggerChange) {
+    //     let editName = document.getElementsByClassName('name')[0];
+
+    //     updateCurrentDocOnChange = triggerChange;
+    //     editName.value = name;
+    //     updateCurrentDocOnChange = triggerChange;
+    // }
 
     function setEditorContent(html, triggerChange) {
         let editor = document.getElementsByClassName('ql-editor')[0];
@@ -56,94 +94,24 @@ function Editor(props) {
         updateCurrentDocOnChange = triggerChange;
     }
 
-    // =====================================
 
-    function changeName(event) {
-        if (event.target !== undefined) {
-
-            let currDoc = { ...props.currentDoc };
-            currDoc.name = event.target.value;
-
-            props.setCurrentDoc(currDoc);
-        }
-    }
-
-    // function changeContent(event, editor) {
-    //     // console.log(event);
-
-    //     // **
-    //     // const editorChildren = document.getElementsByClassName('ql-editor')[0].childNodes;
-    //     // // console.log(editorChildren);
-    //     // let htmlCollection = "";
-    //     // for (let i = 0; i < editorChildren.length; i++) {
-    //     //     console.log(editorChildren[i].outerHTML);
-    //     //     htmlCollection += editorChildren[i];
-    //     // }
-    //     // console.log(htmlCollection);
-    //     // setContent(newContent);
-
-    //     // **
-    //     // OBS: med denna metod (som blir nödvändig med onKeyUp så fås bara p taggen ut)
-    //     // var p = document.querySelector(".ql-editor p");
-    //     // console.log(p);
-    //     // setContent(p);
-    //     // props.setCurrentContent(p);
-
-    //     // **
-    //     setContent(event);
-    //     props.setCurrentContent(event);
-    //     console.log(event);
-
-    //     // **
-    //     // setContent(data);
-    //     // props.setCurrentContent(data);
-    //     // console.log("content changed");
-    // }
+    // NÄR STATET CURRENTDOC ÄNDRAS (LADDAS FRÅN TOOLBAR) SÅ SÄTTS STATES NAME OCH CONTENT
+    // =========================================
 
     useEffect(() => {
-        props.socket.emit("content", content);
-    }, [content]);
-
-
-    useEffect(() => {
-        // console.log(props.currentDoc.content)
         // setEditorName(props.currentDoc.name);
         // setEditorContent(props.currentDoc.content);
 
         setName(props.currentDoc.name);
         setContent(props.currentDoc.content);
 
-        // (async () => {
-        //     // setName(props.currentDoc.name);
-        //     // setContent(props.currentDoc.content);
-
-        //     // setEditor(<CKEditor
-        //     //     editor={ClassicEditor}
-        //     //     data={props.currentDoc.content}
-        //     //     onReady={editor => {
-        //     //         console.log(editor);
-        //     //     }}
-        //     //     onChange={changeContent}
-        //     // // onBlur={(event, editor) => {
-        //     // //     console.log('Blur.', editor);
-        //     // // }}
-        //     // // onFocus={(event, editor) => {
-        //     // //     console.log('Focus.', editor);
-        //     // // }}
-        //     // />);
-        // })();
     }, [props.currentDoc]);
-
-    // useEffect(() => {
-    //     console.log("POOOP");
-    // }, [content]);
 
     return (
         <div>
             <div className="edit-prev-container">
                 <div className="editor">
                     <input type="text" className="name" name="name" value={name} onChange={changeName} />
-                    {/* <ReactQuill theme="snow" value="" class="quilly" /> */}
                     <ReactQuill theme="snow" value={content} onChange={changeContent} class="quilly" />
                     {/* <TrixEditor value={content} /> */}
                     {/* {editor} */}
